d46f03a3ce0207cb88a267c64f01c60433beddc6
Daniel Seisun <daseisun@gmail.com>
2016-05-07 17:17:52 -0700

    Changed tabcmd to use more local directories

--- tabcmd/v9.3/tabcmd.sh
+++ tabcmd/v9.3/tabcmd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx64m -Xss2048k -Djsse.enableSNIExtension=false -Dpid=$$ -Dlog.file=$HOME/.tabcmd/tabcmd.log -Dsession.file=$HOME/.tabcmd/tabcmd-session.xml -Din.progress.dir=/home/dseisun/Novasyte/tabcmd/ -Dconsole.codepage=$LANG -Dconsole.cols=$COLUMNS -cp "/home/dseisun/Novasyte/tabcmd/lib/*" com.tableausoftware.tabcmd.Tabcmd "$@"
+java -Xmx64m -Xss2048k -Djsse.enableSNIExtension=false -Dpid=$$ -Dlog.file=tabcmd.log -Dsession.file=tabcmd-session.xml -Din.progress.dir='./' -Dconsole.codepage=$LANG -Dconsole.cols=$COLUMNS -cp "lib/*" com.tableausoftware.tabcmd.Tabcmd "$@"
